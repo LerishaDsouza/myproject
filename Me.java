122367b129e94450e13bafc00198ee4ef9d9c40e
LerishaDsouza <41681429+LerishaDsouza@users.noreply.github.com>
2020-06-22 04:46:20 -0700

    Add files via upload

---
+++ Me.java
@@ -0,0 +1,17 @@
+class Me{
+
+	public static void main(String[] args){
+
+		for(int i = 2; i < 6; i++){
+		
+			for(int j = 0; j < 5; j++){
+
+				System.out.println("Hello World");
+
+			}
+		}
+
+	}
+
+	
+}
